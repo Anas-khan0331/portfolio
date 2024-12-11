890c56a6f158394d93547c4491cdd2169e74a5b6
AnasKhan0331 <khansaab0331@gmail.com>
2024-12-11 21:05:51 +0500

    Changes Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Anas Khan</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am Front End developer <br className="sm:block hidden" />
+            I am Web Devolper developer <br className="sm:block hidden" />
             and Develop interactive UI interfaces and web applications
           </p>
         </div>
