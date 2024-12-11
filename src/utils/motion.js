6084685df0aaff03cb8c88008ff37b798634e455
AnasKhan0331 <khansaab0331@gmail.com>
2024-12-11 13:00:37 +0500

    second

--- src/utils/motion.js
+++ src/utils/motion.js
@@ -15,7 +15,6 @@ export const textVariant = (delay) => {
     },
   };
 };
-
 export const fadeIn = (direction, type, delay, duration) => {
   return {
     hidden: {
@@ -36,7 +35,6 @@ export const fadeIn = (direction, type, delay, duration) => {
     },
   };
 };
-
 export const zoomIn = (delay, duration) => {
   return {
     hidden: {
